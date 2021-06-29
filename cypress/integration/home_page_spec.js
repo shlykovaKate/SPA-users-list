34a000ce46419e5514b3e1492c553bd08b72e2a4
Kate Shlykova <e2.shlykova@itransition.com>
2021-06-29 18:06:49 +0300

    init cypress

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('The Home Page', () => {
+      it('successfully loads', () => {
+        cy.visit('http://localhost:9000') // change URL to match your dev URL
+        cy.get('a').contains('Users').click()
+        cy.location('pathname').should('include', 'users')
+        cy.get('img')
+          .should('have.length', 10)
+        cy.get('button')
+          .should('have.length', 10)
+          .contains('Delete')
+          .click();
+        cy.get('img')
+          .should('have.length', 9)
+        cy.get('img')
+          .first()
+          .parent()
+          .next()
+          .find('a').click();
+        cy.get('a').contains('Leaders').click()
+        cy.location('pathname').should('include', 'leaders')
+      })
+})
